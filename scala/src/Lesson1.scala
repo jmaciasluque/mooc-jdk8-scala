412359ebbdbabd96adc89ddd9277f22e982de8c3
Juan Jesus Macias Luque <juan.macias.luque@gmail.com>
2015-07-24 19:45:51 +0100

    Initial import: basic implementations of lessons 1 and 2 in Java 8 and Scala

---
+++ scala/src/Lesson1.scala
@@ -0,0 +1,91 @@
+/**
+ * @author Banyuken (banyuken@gmail.com)
+ */
+object Lesson1 {
+  /**
+   * Run the exercises to ensure we got the right answers
+   */
+  def runExercises() {
+    println("JDK 8 Lambdas and Streams MOOC Lesson 1 - A Scala way")
+    println("Running exercise 1 solution...")
+    exercise1()
+    println("Running exercise 2 solution...")
+    exercise2()
+    println("Running exercise 3 solution...")
+    exercise3()
+    println("Running exercise 4 solution...")
+    exercise4()
+    println("Running exercise 5 solution...")
+    exercise5()
+  }
+
+  /** TODO CHANGE THIS DESCRIPTION!
+   * All exercises should be completed using Lambda expressions and the new
+   * methods added to JDK 8 where appropriate. There is no need to use an
+   * explicit loop in any of the code. Use method references rather than full
+   * lambda expressions wherever possible.
+   */
+  /**
+   * Exercise 1
+   *
+   * Create a string that consists of the first letter of each word in the list
+   * of Strings provided.
+   */
+  private def exercise1() {
+    val list = List("alpha", "bravo", "charlie", "delta", "echo", "foxtrot")
+    println(list.map(_.charAt(0)).mkString)
+  }
+
+  /**
+   * Exercise 2
+   *
+   * Remove the words that have odd lengths from the list.
+   */
+  private def exercise2() {
+    List("alpha", "bravo", "charlie", "delta", "echo", "foxtrot")
+      .filterNot(_.length % 2 != 0).foreach(println)
+  }
+
+  /**
+   * Exercise 3
+   *
+   * Replace every word in the list with its upper case equivalent.
+   */
+  private def exercise3() {
+    List("alpha", "bravo", "charlie", "delta", "echo", "foxtrot")
+      .map(_.toUpperCase).foreach(println)
+  }
+
+  /**
+   * Exercise 4
+   *
+   * Convert every key-value pair of the map into a string and append them all
+   * into a single string, in iteration order.
+   */
+  private def exercise4() {
+    val map = Map(("c", 3), ("b", 2), ("a", 1))
+    println(map.toSeq.sortBy(_._1).map(entry => entry._1 + entry._2).mkString)
+  }
+
+  /**
+   * Exercise 5
+   *
+   * Create a new thread that prints the numbers from the list.
+   */
+  private def exercise5() {
+     new Thread(new Runnable {
+      def run() {
+        List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).foreach(print)
+      }
+    }).start()
+  }
+
+  /**
+   * Main entry point for application
+   *
+   * @param args the command line arguments
+   */
+  def main( args:Array[String] ): Unit = {
+    Lesson1.runExercises()
+  }
+}
---
+++ scala/src/Lesson2.scala
@@ -0,0 +1,134 @@
+import scala.io.Source
+
+
+/**
+ * @author Banyuken (banyuken@gmail.com)
+ */
+object Lesson2 {
+  val WORD_REGEXP: String = "[- .:,]+"
+
+  /**
+   * Run the exercises to ensure we got the right answers
+   */
+  def runExercises() {
+    println("JDK 8 Lambdas and Streams MOOC Lesson 2 - A Scala way")
+    println("Running exercise 1 solution...")
+    exercise1()
+    println("Running exercise 2 solution...")
+    exercise2()
+    println("Running exercise 3 solution...")
+    exercise3()
+    println("Running exercise 4 solution...")
+    exercise4()
+    println("Running exercise 5 solution...")
+    exercise5()
+    println("Running exercise 6 solution...")
+    exercise6()
+    println("Running exercise 7 solution...")
+    exercise7()
+  }
+
+  /**
+   * Exercise 1
+   *
+   * Create a new list with all the strings from original list converted to 
+   * lower case and print them out.
+   */
+  private def exercise1() {
+    List("The", "Quick", "BROWN", "Fox", "Jumped", "Over", "The", "LAZY", "DOG")
+      .map(_.toLowerCase).foreach(println)
+  }
+
+  /**
+   * Exercise 2
+   *
+   * Modify exercise 1 so that the new list only contains strings that have an
+   * odd length
+   */
+  private def exercise2() {
+    List("The", "Quick", "BROWN", "Fox", "Jumped", "Over", "The", "LAZY", "DOG")
+      .filter(_.length % 2 != 0).map(_.toLowerCase).foreach(println)
+  }
+
+  /**
+   * Exercise 3
+   *
+   * Join the second, third and forth strings of the list into a single string,
+   * where each word is separated by a hyphen (-). Print the resulting string.
+   */
+  private def exercise3() {
+    val list = List("The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog")
+    println(list.slice(1, 4).mkString("-"))
+  }
+
+  /**
+   * Count the number of lines in the file using the BufferedReader provided
+   */
+  private def exercise4() {
+    // hybrid way
+//        try {
+//          val reader = Files.newBufferedReader(Paths.get("SonnetI.txt"), StandardCharsets.UTF_8)
+//          try {
+//            println(reader.lines.count) // hybrid
+//          } finally {
+//            if (reader != null) reader.close()
+//          }
+//        }
+    // more scala way
+    println(Source.fromFile("SonnetI.txt").getLines().length)
+  }
+
+  /**
+   * Using the BufferedReader to access the file, create a list of words with
+   * no duplicates contained in the file.  Print the words.
+   *
+   * HINT: A regular expression, WORD_REGEXP, is already defined for your use.
+   */
+  private def exercise5() {
+    Source.fromFile("SonnetI.txt")
+      .getLines()
+      .flatMap(line => line.split(WORD_REGEXP))
+      .toSeq
+      .distinct
+      .foreach(println)
+  }
+
+  /**
+   * Using the BufferedReader to access the file create a list of words from 
+   * the file, converted to lower-case and with duplicates removed, which is
+   * sorted by natural order.  Print the contents of the list.
+   */
+  private def exercise6() {
+    Source.fromFile("SonnetI.txt")
+      .getLines()
+      .flatMap(line => line.split(WORD_REGEXP))
+      .map(_.toLowerCase)
+      .toSeq
+      .distinct
+      .sorted
+      .foreach(println)
+  }
+
+  /**
+   * Modify exercise6 so that the words are sorted by length
+   */
+  private def exercise7() {
+    Source.fromFile("SonnetI.txt")
+      .getLines()
+      .flatMap(line => line.split(WORD_REGEXP))
+      .map(_.toLowerCase)
+      .toSeq
+      .distinct
+      .sortBy(_.length)
+      .foreach(println)
+  }
+
+  /**
+   * Main entry point for application
+   *
+   * @param args the command line arguments
+   */
+  def main( args:Array[String] ): Unit = {
+    Lesson2.runExercises()
+  }
+}
